fccefd8ef3a0816157c67e484f57a905ed816c81
Bartosz Majsak <bartosz.majsak@gmail.com>
2012-07-08 13:03:28 +0200

    Fixed failing pagination tests.

--- demo/src/test/java/org/jboss/jdf/ticketmonster/test/rest/EventServiceTest.java
+++ demo/src/test/java/org/jboss/jdf/ticketmonster/test/rest/EventServiceTest.java
@@ -23,68 +23,68 @@ import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
 public class EventServiceTest {
-    
+
     @Deployment
     public static WebArchive deployment() {
         return RESTDeployment.deployment();
     }
-   
+
     @Inject
     private EventService eventService;
-    
+
     @Inject
     private MediaService mediaService;
-    
+
     @Inject
     private MediaManager mediaManager;
-    
+
     @Test
     public void testGetEventById() {
-        
+
         // Test loading a single event
         Event event = eventService.getSingleInstance(1l);
         assertNotNull(event);
         assertEquals("Rock concert of the decade", event.getName());
-        
+
     }
-    
+
     @Test
     public void testGetEventMedia() {
-        
+
         // Test loading a single event
         Event event = eventService.getSingleInstance(1l);
         assertNotNull(event);
-        
+
         MediaPath path = mediaManager.getPath(event.getMediaItem());
         assertNotNull(path);
         assertEquals(MediaType.IMAGE, path.getMediaType());
-        
+
         assertNotNull(mediaService.getMediaContent(event.getMediaItem().getId()));
     }
-    
+
     @Test
     public void testPagination() {
-        
+
         // Test pagination logic
         MultivaluedMap<String, String> queryParameters = new MockMultivaluedMap<String, String>();
-        
-        queryParameters.add("first", "2");
+
+        queryParameters.add("first", "3");
         queryParameters.add("maxResults", "1");
-        
+
         List<Event> events = eventService.getAll(queryParameters);
         assertNotNull(events);
         assertEquals(1, events.size());
         assertEquals("Brazil vs. Italy", events.get(0).getName());
     }
-    
+
     @Test
     public void testGetEventsByCategory() {
-        
+
         // Test getting events by venue
         MultivaluedMap<String, String> queryParameters = new MockMultivaluedMap<String, String>();
-        
+
         queryParameters.add("category", "1");
-        
+
         List<Event> events = eventService.getAll(queryParameters);
         assertNotNull(events);
         assertEquals(5, events.size());
--- demo/src/test/java/org/jboss/jdf/ticketmonster/test/rest/RESTDeployment.java
+++ demo/src/test/java/org/jboss/jdf/ticketmonster/test/rest/RESTDeployment.java
@@ -2,6 +2,7 @@ package org.jboss.jdf.ticketmonster.test.rest;
 
 import org.jboss.jdf.example.ticketmonster.model.Booking;
 import org.jboss.jdf.example.ticketmonster.rest.BaseEntityService;
+import org.jboss.jdf.example.ticketmonster.service.AllocatedSeats;
 import org.jboss.jdf.example.ticketmonster.service.MediaManager;
 import org.jboss.jdf.example.ticketmonster.service.MediaPath;
 import org.jboss.jdf.example.ticketmonster.service.SeatAllocationService;
@@ -14,11 +15,12 @@ public class RESTDeployment {
     public static WebArchive deployment() {
         return TicketMonsterDeployment.deployment()
                 .addPackage(Booking.class.getPackage())
+                .addClass(AllocatedSeats.class)
                 .addPackage(BaseEntityService.class.getPackage())
                 .addPackage(MockMultivaluedMap.class.getPackage())
                 .addClass(SeatAllocationService.class)
                 .addClass(MediaPath.class)
                 .addClass(MediaManager.class);
     }
-    
+
 }
